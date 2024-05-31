c32b9bb24f767cf2c8eff501cc541b44fedbed26
mohammed <moelhasham@uotlook.com>
2024-05-31 16:03:07 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+app.use(express.json());
+
+app.get("/" , async (req,res) => {
+    await res.json("heelo")
+})
+
+app.get("/user" , async (req,res) => {
+    await res.json("heelo user")
+})
+
+app.listen(8800 , (req,res) => {
+    console.log("server is runing")
+})
